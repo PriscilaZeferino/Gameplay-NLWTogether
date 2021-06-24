b797f88afe81525cadd731888d5b9cf2ea7c7fe7
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-06-24 13:21:24 -0300

    Update SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import {StatusBar, Text, View, Image } from 'react-native';
+import {StatusBar, Text, View, Image } from 'react-native'
 
 import {styles} from './styles'
 
 import IllustrationImg from '../../assets/illustration.png'
-import { ButtonIcon } from '../../Components/ButtonComponent';
+import { ButtonIcon } from '../../Components/ButtonComponent'
 export function SignIn() {
   return (
     <View style={styles.container}  >
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
       <Image source={IllustrationImg} style={styles.image} />
       <View style={styles.content} accessible={true} >
-        <Text style={styles.title} accessibilityRole="text">Organize {'\n'} suas jogatinas {'\n'} facilmente</Text>
+        <Text style={styles.title} accessibilityRole="text">
+            Conecte-se {'\n'} 
+            e organize {'\n'} 
+            suas jogatinas {'\n'} 
+            facilmente
+        </Text>
         <Text style={styles.subtitle} accessibilityRole="text">
           Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
