4581adfd1bdf90b0e1d17a2817308983404fcd59
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-06-26 19:08:58 -0300

    Update User Datas

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { View, Text} from 'react-native'
+import { useAuth } from '../../hooks/auth';
 import { Avatar } from '../Avatar'
 import {styles} from './styles'
 
 export function Profile() {
+
+    const {user} = useAuth();
+
     return(
         <View style={styles.container}>
-            <Avatar urlImage="https://github.com/PriscilaZeferino.png"/>
+            <Avatar urlImage={user.avatar}/>
             <View>
                 <View style={styles.user} accessible={true}>
                     <Text 
@@ -19,7 +23,7 @@ export function Profile() {
                         style={styles.username}
                         accessibilityRole="text"
                     >
-                        Priscila
+                       {user.firstName}
                     </Text>
                 </View>
                 <Text 
